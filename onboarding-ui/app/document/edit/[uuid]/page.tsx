d8d40ca5b01b14c03721c4ef7ef4c6e0350e2a06
mrgenco <mrgenco@gmail.com>
2024-01-11 21:39:06 +0300

    SaveDialog component

--- onboarding-ui/app/document/edit/[uuid]/page.tsx
+++ onboarding-ui/app/document/edit/[uuid]/page.tsx
@@ -2,20 +2,16 @@
 import React, { useState } from 'react';
 import Markdown from './components/Markdown';
 import Preview from './components/Preview';
-import SaveDialog from './components/SaveDialog';
 import Button from '../../../ui/button'
 
 export default function Page({ params }: { params: { uuid: string} }) {
     const [markdown, setMarkdown] = useState('');
-    const [saveContent, setSaveContent] = useState(false);
 
     return (
       <div className="flex">
         <Markdown onChange={setMarkdown} />
         <Preview markdown={markdown} />
-        <Button title="Publish" style=""/>
-        <Button title="Save as Draft" style=""/>
-        <SaveDialog/>
+        <Button title="Save Changes" style=""/>
       </div>
     );
 }
