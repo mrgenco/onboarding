4169f9b1f634c30163d37c3bfa9af4dcbfa71f97
mrgenco <mrgenco@gmail.com>
2024-02-16 17:02:56 +0300

    WriteDocument and SaveDocumentDialog components

--- onboarding-ui/src/pages/writedocument/WriteDocument.tsx
+++ onboarding-ui/src/pages/writedocument/WriteDocument.tsx
@@ -1,22 +1,36 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import SaveDocumentDialog from './components/SaveDocumentDialog'
-
-
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 export default function WriteDocument() {
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
-                <SaveDocumentDialog/>
+                <SaveDocumentDialog />
             </Grid>
             <Grid item xs={12} md={6} lg={6}>
-              
+                <TextField
+                    id="filled-multiline-static"
+                    label="Write Your Document Here"
+                    multiline
+                    fullWidth
+                    defaultValue="test"
+                />
             </Grid>
             <Grid item xs={12} md={6} lg={6}>
-                
+                <TextField
+                    fullWidth
+                    id="filled-multiline-static"
+                    label="Preview"
+                    multiline
+                    defaultValue=""
+                />
             </Grid>
-      </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+
+            </Grid>
+        </Grid>
     );
 }
--- onboarding-ui/src/pages/writedocument/components/SaveDocumentDialog.tsx
+++ onboarding-ui/src/pages/writedocument/components/SaveDocumentDialog.tsx
@@ -2,94 +2,120 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import MenuItem from '@mui/material/MenuItem';
+import SendIcon from '@mui/icons-material/Send';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { DialogActions } from '@mui/material';
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const handleDraft = () => {
-    // api call
-  };
 
-  const handlePublish = () => {
-    // api call
-  };
+    const handleSave = () => {
+        // api call
+    };
 
-  return (
-    <React.Fragment>
-      <Button variant="outlined" color='success' onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData as any).entries());
-            const email = formJson.email;
-            console.log(email);
-            handleClose();
-          },
-        }}
-      >
-        <DialogTitle>Save Document</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            You can save your document as draft for later editing, or publish it online.
-          </DialogContentText>
-          
-      <TextField
-        autoFocus
-        required
-        margin="dense"
-        id="title"
-        name="title"
-        label="Title"
-        type="text"
-        fullWidth
-        variant="standard"
-      />
+    const [status, setStatus] = React.useState('');
 
-      <TextField
-        margin="dense"
-        id="categories"
-        name="categories"
-        label="Categories"
-        type="text"
-        fullWidth
-        variant="standard"
-      />
 
-      <TextField
-        margin="dense"
-        id="summary"
-        name="summary"
-        label="Summary"
-        multiline
-        rows={4}
-        fullWidth
-        variant="standard"
-      />
-        </DialogContent>
-        <DialogActions>
-          <Button variant="outlined" color='warning' onClick={handleDraft}>Draft</Button>
-          <Button variant="outlined" color='success' onClick={handlePublish}>Publish</Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Button variant="outlined" color='success' onClick={handleClickOpen}>
+                Open form dialog
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries((formData as any).entries());
+                        const email = formJson.email;
+                        console.log(email);
+                        handleClose();
+                    },
+                }}
+            >
+                <DialogTitle>Save Document</DialogTitle>
+                <DialogContent>
+                    <Box component="form" noValidate onSubmit={handleSave} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="title"
+                                    required
+                                    fullWidth
+                                    id="title"
+                                    label="Document Title"
+                                    autoFocus
+                                />
+
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="summary"
+                                    label="Summary"
+                                    name="summary"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6} >
+                                <TextField
+                                    id="status"
+                                    select
+                                    label="Status"
+                                    defaultValue="0"
+                                    helperText="Please select the document status"
+                                >
+                                    <MenuItem value={0}>Draft</MenuItem>
+                                    <MenuItem value={1}>Publish</MenuItem>
+                                </TextField>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="category"
+                                    label="Categories"
+                                    helperText="Enter categories as comma seperated words. (Ex : Cloud, Programming, Math)"
+                                    name="category"
+                                />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </DialogContent>
+                <DialogActions >
+                    <Grid item alignItems="center">
+                        <Button
+                            type="submit"
+                            size="large"
+                            fullWidth
+                            color='success'
+                            variant="contained"
+                            endIcon={<SendIcon />}
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Save Document
+                        </Button>
+                    </Grid>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
 }
